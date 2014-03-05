69580eac4f7eb3d912e2f437d519b87f537afead
Sandor Weisz <santheo@mac.com>
2014-03-04 19:47:23 -0600

    Removed JS, added back some important elements.

--- js/global.js
+++ js/global.js
@@ -1,15 +1,23 @@
 $(function () {
+    // HACK to quickly fill in Liesl images
+    // --------
     $('section .item').each(function(index, item) {
         $(item).css('background-image', 'url(img/liesl/' + ((index % 11) + 1) + '.jpg)');
     });
 
-    // Make left nav sticky
-    var nav = $('.sidenav');
-    var navTop = nav.offset().top;
-    var navLeft = nav.offset().left;
+    // MAKE LEFT NAV STICKY
+    // --------
+
+    // -- How to get the position of an object
+    // -- relative to top-left corner of window
+    nav.offset().top
+    nav.offset().left
+
+    // -- How to find how many pixels beyond the top
+    // -- of the viewport the window has been scrolled
+    $(window).scrollTop();
+
+    // -- How to handle the scroll event
     $(window).scroll(function() {
-        var makeItStick = navTop < $(window).scrollTop();
-        nav.toggleClass('stuck', makeItStick);
-        nav.css('left', makeItStick ? navLeft : 0);
     });
 });
