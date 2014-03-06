35c8d659fd879344416874b5c3656404cdcf51f0
Sandor Weisz <sandor@gmail.com>
2014-03-06 14:17:24 -0600

    Added log function.

--- js/global.js
+++ js/global.js
@@ -1,9 +1,16 @@
 $(function () {
+    var log = function() {
+        console.log(arguments);
+    };
+
+    // HACK to quickly fill in Liesl images
+    // --------
     $('section .item').each(function(index, item) {
         $(item).css('background-image', 'url(img/liesl/' + ((index % 11) + 1) + '.jpg)');
     });
 
-    // Make left nav sticky
+    // MAKE LEFT NAV STICKY
+    // --------
     var nav = $('.sidenav');
     var navTop = nav.offset().top;
     var navLeft = nav.offset().left;
